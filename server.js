998b19aac84beb027126ab0a58891f8f43d2e078
Bibars-shouk <bibars.shouk1@gmail.com>
2021-09-04 06:12:14 +0300

    install mongoose, create connection to MongoDB

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 
-// Route files
-const medicalReports = require("./routes/medicalReports");
+// DB config
+const connectDB = require("./config/db");
 
 dotenv.config({ path: "./config/config.env" });
+
+// connect to DB
+connectDB();
+
+// Route files
+const medicalReports = require("./routes/medicalReports");
 
 const app = express();
 
@@ -17,8 +23,18 @@ app.use("/api/medical-reports", medicalReports);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT} `
   );
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server and exit process
+  server.close(() => {
+    // exit with failure
+    process.exit(1);
+  });
 });
